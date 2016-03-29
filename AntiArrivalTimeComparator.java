5218488930a23dd6107c93aaef916636163f539e
renandincer <renandincer@gmail.com>
2016-03-29 19:50:13 -0400

    adding report

--- AntiArrivalTimeComparator.java
+++ AntiArrivalTimeComparator.java
@@ -1,13 +1,13 @@
 /**
- * Everything is equal!
+ * Compares two processes based on their arrival time (but the other way around)
  * @author Renan Dincer and Ivan Evtimov
  */
 
 import java.util.Comparator;
 
-public class BullshitComparator implements Comparator<Process>{
+public class AntiArrivalTimeComparator implements Comparator<Process>{
     public int compare(Process o1, Process o2){
-        return 0;
+        return o2.arrivalTime() - o1.arrivalTime();
     }
 
     public boolean equals(Object obj){
--- CustomScheduler.java
+++ CustomScheduler.java
@@ -1,23 +1,29 @@
 /**
  * Scheduler that implements our proprietory super secret custom scheduling algorithm
+ * It is basically Last Come First Serve - Optimizing for response time.
  * @author Renan Dincer and Ivan Evtimov
  */
 
+import java.util.PriorityQueue;
 public class CustomScheduler extends Scheduler {
-    private PriorityComparator comp;
 
     public CustomScheduler()
     {
+		current = null;
+		rq = new PriorityQueue<Process>(new AntiArrivalTimeComparator());
 
     }
 
     public boolean contextSwitchable()
     {
-        return true;   
+		Process temp = rq.peek();
+        if(current == null){
+			return true;
+		} else if (temp == null || current.arrivalTime() < temp.arrivalTime()){
+			return true;
+		} else {
+			return false;
+		}
     }
 
-    public void updateRQ()
-    {
-
-    }
 }
